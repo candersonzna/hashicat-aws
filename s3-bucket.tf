506518fc51221dfecaf822094b66ca108f8b6ce8
Colin Anderson <colin.anderson1@zurichna.com>
2022-04-21 20:26:45 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,8 +1,12 @@
-module "s3-bucket" {
-  source  = "app.terraform.io/ZNA-Colin/s3-bucket/aws"
+module "s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
   version = "2.8.0"
-  
-  bucket_prefix = var.prefix
-  bucket = "Something"
-  acl = "private"
+  bucket_prefix = "Colin Anderson"
+  bucket = "my-s3-bucket"
+  acl    = "private"
+
+  versioning = {
+    enabled = true
+  }
+
 }
